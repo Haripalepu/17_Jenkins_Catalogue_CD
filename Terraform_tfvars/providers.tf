4667c93a642d44d191a4ee10f8712f72920a297d
hari <hari@gmail.com>
2024-02-11 01:29:04 +0530

    provideres s3

--- Terraform_tfvars/providers.tf
+++ Terraform_tfvars/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "s3" { 
      bucket = "terraform-env--dev"
-     key    = "Catalogue_CD"
+     key    = "17_Catalogue_CD"
      region = "us-east-1"
      dynamodb_table = "Terraform-env-dev"
   }
