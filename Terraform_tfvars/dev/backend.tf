6f32f688fcfa0c588514e4c7930d6879b48986c8
hari <hari@gmail.com>
2024-02-11 03:30:00 +0530

    component

--- Terraform_tfvars/dev/backend.tf
+++ Terraform_tfvars/dev/backend.tf
@@ -1,6 +1,6 @@
 
   
     bucket = "terraform-env--dev" #Create an S3 bucket with same name 
-    key    = "Catalogue_CD-dev"
+    key    = "Catalogue_CD-dev-02"
     region = "us-east-1"
     dynamodb_table = "Terraform-env-dev" #DynamoDB name
