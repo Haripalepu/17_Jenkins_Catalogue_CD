b1eaad7e5ed6c0fe7eac75e0e374e20a3c94493d
hari <hari@gmail.com>
2024-02-10 21:00:22 +0530

    Update main.tf

---
+++ Terraform_tfvars/dev/backend.tf
@@ -0,0 +1,6 @@
+
+  
+    bucket = "terraform-env--dev" #Create an S3 bucket with same name 
+    key    = "Catalogue_CD-dev"
+    region = "us-east-1"
+    dynamodb_table = "Terraform-env-dev" #DynamoDB name
